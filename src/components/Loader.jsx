50057af9a11b198df00238076dc2a8860941d838
Maadeha shaikh <maadeham@gmail.com>
2024-09-15 23:55:22 +0500

    update

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Loader.css"
+const Loader = () => {
+  return (
+    <div style={{display:"flex", alignItems:"center",justifyContent:"center",marginTop:"100px",padding:"30px"}}>
+      <div class="loader"></div>
+      </div>
+   
+  )
+}
+
+export default Loader
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,34 +1,42 @@
 import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+import  Loader  from './Loader';
 
 const Pricing = () => {
-  const [products,setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loader,setLoader] = useState(false);
   const fetchProducts = async () => {
+    setLoader(true);
+    console.log(loader);
     const response = await axios.get("https://fakestoreapi.com/products");
     setProducts(response.data);
+    setLoader(false);
   }
-  useEffect(()=>{
+  useEffect(() => {
     fetchProducts();
-  },[]);
-  console.log(products);
+  }, []);
   return (
-    <div>
-    {
-      products.map((product)=>{
-      return (
-        <>
-        <h1>{product.title}</h1>
-        <h2>{product.category}</h2>
-        <p>{product.description}</p>
-        <img src={product.image} width={"200px"}/>
-        <p>{product.price}$</p>
-        <p>{product.rating.rate}</p>
-        </> 
-      )
-      })
-}
-    </div>
+    <>
+    {loader ? <Loader/> : (<div style={{display:"flex",flexWrap:"wrap",marginLeft:"70px"}}>
+      {
+        products.map((product) => {
+          return (
+            <>
+            <div style={{width:"340px",backgroundColor:"#6438A4",height:"59vh", border:"2px solid black",padding:"10px 20px",margin:"10px",borderRadius:"30px",color:"white"}}>
+              <h2>{product.title.slice(0,10)}...</h2>
+              <h4>{product.category}</h4>
+              <p>{product.description.slice(0,90)}...Read More</p>
+              <img src={product.image} width={"100px"} height={"100px"}/><br />
+              <span> Price :{product.price}$</span><br />
+              <span>Rating :{product.rating.rate}</span>
+            </div>
+            </>
+          )
+        })
+      }
+    </div>)}
+    </>
   )
 }
 
