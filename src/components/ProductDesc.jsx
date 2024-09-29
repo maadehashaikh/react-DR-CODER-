e9364b42b95c1658b394cc735275b1ce7b494a0b
maadehashaikh <maadeham@gmail.com>
2024-09-29 20:38:33 +0500

    updated

--- src/components/ProductDesc.jsx
+++ src/components/ProductDesc.jsx
@@ -1,23 +1,23 @@
 import axios from 'axios';
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import Pricing from './Pricing';
 const ProductDesc = () => {
-  const[product,setProduct] = useState(null);
+  const [product, setProduct] = useState(null);
   const params = useParams();
-  const fetchProduct = async() => {
-    const response = await axios.get (`https://fakestoreapi.com/products/${params.id}`);
+  const fetchProduct = async () => {
+    const response = await axios.get(`https://fakestoreapi.com/products/${params.id}`);
     setProduct(response.data);
   }
-  useEffect(()=>{
-   fetchProduct();
-  },[]);
+  useEffect(() => {
+    fetchProduct();
+  }, []);
   return (
     <div>
       <div className='container'>
         <div className='rows'>
           <div className='col-lg-6'>
-            <img src={product.image} width={"300px"} height={"300px"}/>
+            <img src={product.image} width={"300px"} height={"300px"} />
           </div>
           <div className='col-lg-6'>
             <h3>{product.title}</h3>
